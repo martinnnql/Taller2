948bf33cff18b0d6fa3376f2a3ce5f0068e67c09
martinnnql <martinarancibia20@gmail.com>
2026-05-03 17:01:26 -0400

    clases Pokemon y Gimnasio añadidas

---
+++ Taller2_Pokemon/dominio/Pokemon.java
@@ -0,0 +1,144 @@
+package dominio;
+
+public class Pokemon {
+	private String nombre;
+	private String habitat;
+	private double porcentajeAparicion;
+	private int vida;
+	private int ataque;
+	private int defensa;
+	private int ataqueEspecial;
+	private int defensaEspecial;
+	private int velocidad;
+	private String tipo;
+	private String estado;
+	
+	
+	public Pokemon(String nombre, String habitat, double porcentajeAparicion, int vida, int ataque, int defensa,
+			int ataqueEspecial, int defensaEspecial, int velocidad, String tipo) {
+		super();
+		this.nombre = nombre;
+		this.habitat = habitat;
+		this.porcentajeAparicion = porcentajeAparicion;
+		this.vida = vida;
+		this.ataque = ataque;
+		this.defensa = defensa;
+		this.ataqueEspecial = ataqueEspecial;
+		this.defensaEspecial = defensaEspecial;
+		this.velocidad = velocidad;
+		this.tipo = tipo;
+		
+	}
+
+
+	public boolean setEstado() {
+		if (estado.equals("Vivo")) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getHabitat() {
+		return habitat;
+	}
+
+
+	public void setHabitat(String habitat) {
+		this.habitat = habitat;
+	}
+
+
+	public double getPorcentajeAparicion() {
+		return porcentajeAparicion;
+	}
+
+
+	public void setPorcentajeAparicion(double porcentajeAparicion) {
+		this.porcentajeAparicion = porcentajeAparicion;
+	}
+
+
+	public int getVida() {
+		return vida;
+	}
+
+
+	public void setVida(int vida) {
+		this.vida = vida;
+	}
+
+
+	public int getAtaque() {
+		return ataque;
+	}
+
+
+	public void setAtaque(int ataque) {
+		this.ataque = ataque;
+	}
+
+
+	public int getDefensa() {
+		return defensa;
+	}
+
+
+	public void setDefensa(int defensa) {
+		this.defensa = defensa;
+	}
+
+
+	public int getAtaqueEspecial() {
+		return ataqueEspecial;
+	}
+
+
+	public void setAtaqueEspecial(int ataqueEspecial) {
+		this.ataqueEspecial = ataqueEspecial;
+	}
+
+
+	public int getDefensaEspecial() {
+		return defensaEspecial;
+	}
+
+
+	public void setDefensaEspecial(int defensaEspecial) {
+		this.defensaEspecial = defensaEspecial;
+	}
+
+
+	public int getVelocidad() {
+		return velocidad;
+	}
+
+
+	public void setVelocidad(int velocidad) {
+		this.velocidad = velocidad;
+	}
+
+
+	public String getTipo() {
+		return tipo;
+	}
+
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	
+	
+}
